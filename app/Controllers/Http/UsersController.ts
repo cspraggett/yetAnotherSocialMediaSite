a1f72925ab8833ba00e003e968b59a24e45b74f7
cspraggett <cspraggett@gmail.com>
2021-08-11 14:24:01 -0400

    Move newUser validator to it/'s own class

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,37 +1,23 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import CreateUser from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
   public async index(ctx: HttpContextContract) {
-    // const user = await User.first()
     const user = await User.findBy('email', 'jdoe@gmail.com')
     ctx.response.send(user)
   }
 
-  public async store(ctx: HttpContextContract) {
-    const newUserSchema = schema.create({
-      email: schema.string({ trim: true }, [
-        rules.email(),
-        rules.unique({ table: 'users', column: 'email' }),
-      ]),
-      first_name: schema.string({ trim: true }),
-      last_name: schema.string({ trim: true }),
-      avatar_url: schema.string.optional(),
-      password: schema.string({ trim: false }),
-    })
-
+  public async store({ request, response }: HttpContextContract) {
     try {
-      const payload = await ctx.request.validate({
-        schema: newUserSchema,
-      })
+      const payload = await request.validate(CreateUser)
 
       const user = new User()
 
       await user.fill(payload).save()
-      ctx.response.send(user)
+      response.send(user)
     } catch (err) {
-      ctx.response.badRequest(err.messages)
+      response.badRequest(err.messages)
     }
   }
 }
---
+++ app/Validators/CreateUserValidator.ts
@@ -0,0 +1,33 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CreateUserValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string({ trim: true }, [
+      rules.email(),
+      rules.unique({ table: 'users', column: 'email' }),
+    ]),
+    first_name: schema.string({ trim: true }),
+    last_name: schema.string({ trim: true }),
+    avatar_url: schema.string.optional(),
+    password: schema.string({ trim: false }),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages = {
+    'email.unique': 'Email is already being used',
+    'email.email': 'Email is not in proper format',
+  }
+}
