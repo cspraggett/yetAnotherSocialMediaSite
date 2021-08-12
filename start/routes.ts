18fabf4f2c5e4f95ffe63e8d30681070e5611c50
cspraggett <cspraggett@gmail.com>
2021-08-11 23:16:51 -0400

    Clean up User controller and related routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,23 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('login', async ({ auth, request, response }) => {
@@ -38,12 +18,12 @@ Route.post('/logout', async ({ auth, response }) => {
   return response.send('good bye')
 })
 
-Route.post('users', 'UsersController.store')
+Route.post('register', 'UsersController.store')
 
 Route.group(() => {
   Route.get('/', async () => {
     return { hello: 'world' }
   })
 
-  Route.get('users', 'UsersController.index')
+  Route.get('users/:id', 'UsersController.show')
 }).middleware('auth')
