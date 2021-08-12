8634c4742313255b849a3ce07c96857659c56e5c
cspraggett <cspraggett@gmail.com>
2021-08-11 23:28:48 -0400

    Move login and logout to sessionController

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -1,4 +1,21 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class SessionsController {
+  public async login({ request, response, auth }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    try {
+      await auth.use('web').attempt(email, password)
+      response.send('all good!')
+    } catch (err) {
+      console.log(err)
+      return response.badRequest(err)
+    }
+  }
+
+  public async logout({ auth, response }: HttpContextContract) {
+    await auth.use('web').logout()
+    return response.send('good bye')
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,29 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('login', async ({ auth, request, response }) => {
-  const email = request.input('email')
-  const password = request.input('password')
-
-  try {
-    await auth.use('web').attempt(email, password)
-    response.send('all good!')
-  } catch (err) {
-    console.log(err)
-    return response.badRequest(err)
-  }
-})
-
-Route.post('/logout', async ({ auth, response }) => {
-  await auth.use('web').logout()
-  return response.send('good bye')
-})
+Route.post('login', 'SessionsController.login')
 
 Route.post('register', 'UsersController.store')
 
 Route.group(() => {
-  Route.get('/', async () => {
-    return { hello: 'world' }
-  })
+  Route.post('logout', 'SessionsController.logout')
 
   Route.get('users/:id', 'UsersController.show')
 }).middleware('auth')
